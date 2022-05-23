dd5402569313d9698ec8abfd230bc95d8bc3d978
Secstudent <Secstudent@stjude.local>
2022-05-24 02:11:06 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong>PROUDLY DONE BY SMITH STUDENTS</strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong>@PROUDLY DONE BY SMITH STUDENTS</strong>
+    </div>
+    <!-- /.container -->
+</footer>
